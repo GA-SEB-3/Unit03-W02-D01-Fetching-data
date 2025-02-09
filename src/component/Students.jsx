957cca00065fab9f418604faec76d0ef997b2dd9
omarakamal <omaraakamal@gmail.com>
2025-02-09 18:51:15 +0300

    updated code

--- src/component/Students.jsx
+++ src/component/Students.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Students() {
+
+    console.log("component renders")
+    
+    const [students,setStudents] = useState([])
 
     async function getAllStudents(){
         const allStudents = await axios.get("https://omar-ga-class.onrender.com/students")
         console.log(allStudents.data)
+        setStudents(allStudents.data)
     }
 
-    getAllStudents()
+    // getAllStudents()
+
+    // useEffect() is a function
+    // takes 2 arguments: 
+    //                  1. function
+    //                  2. array
+
+    useEffect(()=>{getAllStudents()},[])
 
   return (
     <div>
         <h1>All Students</h1>
+
+        {students.map((oneStudent)=>
+        <div key={oneStudent.id}>
+            <h2>{oneStudent.studentName}</h2>
+        </div>
+        )}
       
     </div>
   )
