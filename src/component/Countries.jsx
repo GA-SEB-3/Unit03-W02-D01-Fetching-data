e082306146554da14c3a885fe7b7d8f064e4ed49
omarakamal <omaraakamal@gmail.com>
2025-02-09 18:54:27 +0300

    updated code

--- src/component/Countries.jsx
+++ src/component/Countries.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function Countries() {
+    // Exercise 1:
+    // get all the countries in the rest countries API: https://restcountries.com/v3.1/name/all
+    // store them in a state
+    // map through all the values and show just the name of the country
+    // BONUS: show the capital, population, and flag
   return (
     <div>
       
